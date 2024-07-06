2b3b874a62d2be8f0400755088fac9c7837e5471
lucyjack886 <144027822+lucyjack886@users.noreply.github.com>
2024-07-06 14:26:26 +0800

    Create xrayr_unlock.sh

---
+++ xrayr_unlock.sh
@@ -0,0 +1,86 @@
+
+# 创建配置文件 route.json
+cat <<EOF > route.json
+{
+  "domainStrategy": "IPOnDemand",
+  "rules": [
+    {
+      "type": "field",
+      "outboundTag": "block",
+      "ip": [
+        "geoip:private"
+      ]
+    },
+    {
+      "type": "field",
+      "outboundTag": "block",
+      "protocol": [
+        "bittorrent"
+      ]
+    },
+    {
+      "type": "field",
+      "outboundTag": "socks5-warp",
+      "domain": ["chatgpt.com","openai.com"]
+    },
+    {
+      "type": "field",
+      "outboundTag": "socks5-warp-ipv6",
+      "domain": [
+        "geosite:netflix","geosite:disney","geosite:hbo"
+      ]
+    },
+    {
+      "type": "field",
+      "outboundTag": "IPv4_out",
+      "network": "udp,tcp"
+    }
+  ]
+}
+EOF
+
+# 创建配置文件 custom_outbound.json
+cat <<EOF > custom_outbound.json
+[
+  {
+    "tag": "IPv4_out",
+    "protocol": "freedom",
+    "settings": {}
+  },
+  {
+    "tag": "IPv6_out",
+    "protocol": "freedom",
+    "settings": {
+      "domainStrategy": "UseIPv6"
+    }
+  },
+  {
+    "tag": "socks5-warp",
+    "protocol": "socks",
+    "settings": {
+      "servers": [
+        {
+          "address": "127.0.0.1",
+          "port": 40000 
+        }
+      ]
+    }
+  },
+  {
+    "tag": "socks5-warp-ipv6",
+    "protocol": "socks",
+    "settings": {
+      "servers": [
+        {
+          "address": "::1",
+          "port": 40000
+        }
+      ]
+    }
+  },
+  {
+    "protocol": "blackhole",
+    "tag": "block"
+  }
+]
+EOF
